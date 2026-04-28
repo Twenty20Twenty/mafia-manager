1598016bca835fd529e8584d09694dc4454ba7a7
Twenty20Twenty <nikita.lipatow2014@yandex.ru>
2026-04-28 23:22:39 +0700

    server fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,8 +5,6 @@ import App from './App.jsx'
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/notifications/styles.css';
-
-// Глобальные стили приложения (включая поддержку двух тем)
 import './index.css';
 
 import { AuthProvider } from './context/AuthContext';
